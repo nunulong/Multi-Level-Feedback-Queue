d2d37f3f1aa3fa6f41ded68714cabe842968837e
Ting Wang <felix.wang.1026@gmail.com>
2018-03-14 20:12:33 -0400

    Finished the MLFQ

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -1,20 +1,22 @@
-const Queue = require('./Queue'); 
-const { 
-    QueueType,
-    PRIORITY_LEVELS,
-} = require('./constants/index');
+const Queue = require('./Queue');
+const { QueueType, PRIORITY_LEVELS } = require('./constants/index');
 
 // A class representing the scheduler
-// It holds a single blocking queue for blocking processes and three running queues 
+// It holds a single blocking queue for blocking processes and three running queues
 // for non-blocking processes
-class Scheduler { 
-    constructor() { 
+class Scheduler {
+    constructor() {
         this.clock = Date.now();
         this.blockingQueue = new Queue(this, 50, 0, QueueType.BLOCKING_QUEUE);
         this.runningQueues = [];
 
         for (let i = 0; i < PRIORITY_LEVELS; i++) {
-            this.runningQueues[i] = new Queue(this, 10 + i * 20, i, QueueType.CPU_QUEUE);
+            this.runningQueues[i] = new Queue(
+                this,
+                10 + i * 20,
+                i,
+                QueueType.CPU_QUEUE
+            );
         }
     }
 
@@ -29,17 +31,43 @@ class Scheduler {
     // If yes, then break out of the infinite loop
     // Otherwise, perform another loop iteration
     run() {
+        while (true) {
+            const currentTime = Date.now();
+            const workTime = currentTime - this.clock;
+            this.clock = currentTime;
 
+            if (!this.blockingQueue.isEmpty()) {
+                this.blockingQueue.doBlockingWork(workTime);
+            }
+
+            for (let i = 0; i < PRIORITY_LEVELS; i++) {
+                const queue = this.runningQueues[i];
+                if (!queue.isEmpty()) {
+                    queue.doCPUWork(workTime);
+                    break;
+                }
+            }
+
+            if (this.allEmpty()) {
+                break;
+            }
+        }
     }
 
-    // Checks that all queues have no processes 
+    // Checks that all queues have no processes
     allEmpty() {
+        for (let i = 0; i < PRIORITY_LEVELS; i++) {
+            if (!this.runningQueues[i].isEmpty()) {
+                return false;
+            }
+        }
 
+        return this.blockingQueue.isEmpty();
     }
 
     // Adds a new process to the highest priority level running queue
     addNewProcess(process) {
-
+        this.runningQueues[0].enqueue(process);
     }
 
     // The scheduler's interrupt handler that receives a queue, a process, and an interrupt string
@@ -49,7 +77,27 @@ class Scheduler {
     // If it is a running queue, add the process to the next lower priority queue, or back into itself if it is already in the lowest priority queue
     // If it is a blocking queue, add the process back to the blocking queue
     handleInterrupt(queue, process, interrupt) {
-
+        switch (interrupt) {
+            case 'PROCESS_BLOCKED':
+                this.blockingQueue.enqueue(process);
+                break;
+            case 'PROCESS_READY':
+                this.addNewProcess(process);
+                break;
+            case 'LOWER_PRIORITY':
+                if (queue.getQueueType() === QueueType.CPU_QUEUE) {
+                    const priorityLevel = Math.min(
+                        PRIORITY_LEVELS - 1,
+                        queue.getPriorityLevel() + 1
+                    );
+                    this.runningQueues[priorityLevel].enqueue(process);
+                } else {
+                    this.blockingQueue.enqueue(process);
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     // Private function used for testing; DO NOT MODIFY
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,12 @@
 const SchedulerInterrupt = {
     PROCESS_BLOCKED: 'PROCESS_BLOCKED',
     PROCESS_READY: 'PROCESS_READY',
-    LOWER_PRIORITY: 'LOWER_PRIORITY',
+    LOWER_PRIORITY: 'LOWER_PRIORITY'
 };
 
 const QueueType = {
     CPU_QUEUE: 'CPU_QUEUE',
-    BLOCKING_QUEUE: 'BLOCKING_QUEUE',
+    BLOCKING_QUEUE: 'BLOCKING_QUEUE'
 };
 
 const PRIORITY_LEVELS = 3;
@@ -14,5 +14,5 @@ const PRIORITY_LEVELS = 3;
 module.exports = {
     SchedulerInterrupt,
     QueueType,
-    PRIORITY_LEVELS,
+    PRIORITY_LEVELS
 };
